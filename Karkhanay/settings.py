c7dcc74aa73693b1c9644ae241c49bd85b5e1980
FahadZaheerfzr <fahadzaheer19@gmail.com>
2020-10-12 17:13:50 +0500

    Customer, Vendor and Store Model Created

--- Karkhanay/settings.py
+++ Karkhanay/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'E_Commerce.apps.ECommerceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
