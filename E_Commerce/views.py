6813971797a87f8c7be3c2f7e16d54cb7b4dd421
Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2021-01-15 21:29:37 +0500

    modified .gitignore

--- E_Commerce/views.py
+++ E_Commerce/views.py
@@ -463,7 +463,7 @@ def store_registration(request):
 
 
 def temp(request):
-    return render(request, "E_Commerce/SingleProduct.html")
+    return render(request, "E_Commerce/AddProducts.html")
 
 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
