c939e136f4230ee3bb875b06e3755e66dc3ea692
FahadZaheerfzr <fahadzaheer19@gmail.com>
2021-01-15 16:55:53 +0500

    Store Registration Changed and Add Product Improved

--- E_Commerce/models/Product.py
+++ E_Commerce/models/Product.py
@@ -8,33 +8,34 @@ class Product(TimeStampedModel):
     product_category = models.ForeignKey(Product_Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     price = models.FloatField(max_length=100)
-    discounted_price = models.FloatField(max_length=100)
     short_description = models.CharField(max_length=500, null=True)
     description = models.CharField(max_length=1000)
-    stock_count = models.IntegerField(null=True)
-  #  sale_from = models.DateField()
- #   sale_till = models.DateField()
-  #  sale_percent = models.IntegerField()
+    stock_count = models.IntegerField()
+    sale_from = models.CharField(max_length=100, null=True, blank=True)
+    sale_till = models.CharField(max_length=100, null=True, blank=True)
+    sale_percent = models.IntegerField(null=True, blank=True)
     featured = models.BooleanField(verbose_name='featured_product', default=False)
     tag = models.ManyToManyField(Tag)
     stock_status = models.CharField(max_length=20)
     one_quantity_sale = models.BooleanField(verbose_name="one_quantity_name", default=False)
-    stock_management = models.BooleanField(verbose_name="stock_management", default=False)
+    stock_management = models.BooleanField(verbose_name="stock_management", default=True)
     weight = models.FloatField(max_length=10, null=True)
     sku = models.CharField(max_length=50, null=True)
     status = models.CharField(max_length=50, null=True)
     visibility = models.CharField(max_length=50, null=True)
-    purchase_note = models.CharField(max_length=100,null=True)
+    purchase_note = models.CharField(max_length=100, null=True)
     featured_image = models.ImageField(upload_to="products/")
 
-    def create_product(self, store, name, category, price, discounted_price, description, s_description, stk_cnt):
+    def create_product(self, store, name, category, price, discounted_price, description, s_description, stk_cnt,
+                       sale_from=None, sale_till=None):
         self.name = name
         self.product_category = category
         self.price = price
         self.description = description
-        self.discounted_price = discounted_price
+        self.sale_percent = discounted_price
         self.short_description = s_description
         self.stock_count = stk_cnt
         self.store = store
+        self.sale_from = sale_from
+        self.sale_till = sale_till
         self.save()
-
--- E_Commerce/views.py
+++ E_Commerce/views.py
@@ -17,6 +17,7 @@ from django.contrib.auth.hashers import make_password, check_password
 from .tokens import account_activation_token, password_reset_token
 
 
+
 def home(request):
     prod = Product.objects.all()
     cat = Product_Category.objects.filter(parent_cat=None)
@@ -38,8 +39,7 @@ def cart(request):
         ls = []
         for p in temp:
             product = Product.objects.get(pk=p)
-            if product not in ls:
-                ls.append(product)
+            ls.append(product)
         ven = {
             "product": ls
         }
@@ -456,7 +456,7 @@ def store_registration(request):
 
 
 def temp(request):
-    return render(request, "E_Commerce/StoreSetup.html")
+    return render(request, "E_Commerce/AddProducts.html")
 
 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
@@ -511,14 +511,6 @@ def withdraw_vendor(request):
 
 def returns_vendor(request):
     return render(request, 'E_Commerce/ReturnsVendor.html')
-
-
-def staff_vendor(request):
-    return render(request, 'E_Commerce/StaffVendor.html')
-
-
-def add_staff(request):
-    return render(request, 'E_Commerce/AddStaff.html')
 
 
 def followers_vendor(request):
@@ -594,16 +586,21 @@ def create_product(request):
         prod = Product()
         user = Vendor.objects.get(encrypted_id=request.session["authenticated"])
         store = Store.objects.get(vendor=user)
-
         name = request.POST["ProductName"]
         price = request.POST["price"]
-        discounted = request.POST["discount"]
+        if request.POST['discount'] != "":
+            discounted = request.POST["discount"]
+        else:
+            discounted = None
         s_des = request.POST["shortDescription"]
         des = request.POST["description"]
         category = Product_Category.objects.get(name=request.POST["categories"])
         tag = request.POST.getlist("tags")
         stock_count = request.POST["stock_count"]
-        prod.create_product(store, name, category, price, discounted, des, s_des, stock_count)
+        sale_from = request.POST["sale_from"]
+        sale_till = request.POST["sale_till"]
+
+        prod.create_product(store, name, category, price, discounted, des, s_des, stock_count,sale_from,sale_till)
 
         tags = []
         for t in tag:
@@ -619,6 +616,7 @@ def create_product(request):
             img.save()
 
         request.session["product"] = prod.pk
+        messages.success(request, "Product Created Successfully!")
         return render(request, 'E_Commerce/EditProduct.html')
 
 
@@ -641,14 +639,15 @@ def edit_product(request):
         # product.tag.set(tags)
         product.sku = request.POST["sku"]
         product.stock_status = request.POST["status"]
-        product.one_quantity_sale = request.POST["one_quantity"] == "on"
-        product.stock_management = request.POST["stock_management"] == "on"
+        if request.POST["one_quantity"]:
+            product.one_quantity_sale = request.POST["one_quantity"]
+            product.stock_management = request.POST["stock_management"]
         product.weight = request.POST["weight"]
         product.status = request.POST["productStatus"]
         product.visibility = request.POST["visibility"]
         product.purchase_note = request.POST["purchaseNote"]
         product.save()
-        messages.success(request, "Product Created Successfully!")
+
 
         del request.session["product"]
         return redirect("/products")
@@ -717,8 +716,9 @@ def tag_create(request):
 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def add_to_cart(request, prod_pk):
-    temp_cart = []
-    temp_cart.append(prod_pk)
+    temp_cart = request.session["product"]
+    if prod_pk not in temp_cart:
+        temp_cart.append(prod_pk)
     request.session["product"] = temp_cart
     return redirect("/")
 
